dba0c0445b5fd5009dbcf50d9a80210eb9bd5d0c
Jayde Callejas <jcallejasrodriguez@gmail.com>
2026-03-12 13:00:34 -0400

    Began implementing tractor beam and resource items

---
+++ Assets/Scripts/Pilot/ResourceItem.cs
@@ -0,0 +1,44 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Pilot {
+
+    public enum ResourceType {
+
+        DEBRIS,
+        FUEL,
+        AMMO,
+        HULL
+
+    }
+
+    public class ResourceItem : MonoBehaviour {
+
+        [SerializeField] private ResourceType type;
+        [SerializeField] private GameObject cube;
+        [SerializeField] private TextMeshPro label;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start() {
+            label.text = type.ToString();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            cube.transform.Rotate(Vector3.up * Time.deltaTime, Space.Self);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player"))
+                PickUp();
+        }
+
+        private void PickUp() {
+            Destroy(gameObject);
+            // TODO: Integrate Engineer system here
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Pilot/ShipTractor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Pilot {
+
+    public class ShipTractor : MonoBehaviour {
+
+        [SerializeField] private float rangeDistance;
+        [SerializeField] private float rangeAngle;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+
+    }
+
+}
