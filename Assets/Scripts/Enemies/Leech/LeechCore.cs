82505970a2f087b5c50778ff1b2b185b63a6bfae
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-04-04 09:51:43 -0400

    Began Leech enemy implementation: Created behavior tree, implemented preliminary tree structure, created LeechCore.cs script, created prefab

---
+++ Assets/Scripts/Enemies/Leech/LeechCore.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Enemies.Leech {
+
+    public class LeechCore : MonoBehaviour {
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+
+    }
+
+}
