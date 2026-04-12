d87268e99a4c9e0da5c0bf1ac2bc832fdc520352
Jayde Iris Callejas <jcallejasrodriguez@gmail.com>
2026-04-11 23:21:33 -0400

    Added preprocessor directives to ShipTractor.cs script to exclude editor-only code from builds

--- Assets/Scripts/Pilot/ShipTractor.cs
+++ Assets/Scripts/Pilot/ShipTractor.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using Logitech;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Pilot {
 
@@ -59,6 +61,7 @@ namespace Pilot {
 
     }
 
+    #if UNITY_EDITOR
     [CustomEditor(typeof(ShipTractor))]
     public class ShipTractorGUI : Editor {
 
@@ -70,5 +73,6 @@ namespace Pilot {
         }
 
     }
+    #endif
 
 }
